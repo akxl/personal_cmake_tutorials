152e4a9e69ff30c87aa30cdd4576391d5e495c09
Aaron Leong <aaron@example.com>
2018-12-19 21:53:06 +0000

    seems to be working

---
+++ part_1b/MathFunctions/mysqrt.cxx
@@ -0,0 +1,12 @@
+#include "MathFunctions.h"
+
+double mysqrt(double x) {
+    double x0 = 0.0;
+    double x1 = 5.0;
+    double epsilon = 1.0e-5;
+    while (abs(x1-x0) > epsilon) {
+        x0 = x1;
+        x1 = x0 - ((pow(x0, 2.0) - x)/(x*x0));
+    }
+    return x1;
+}
